26a6db7ee3cac9f8e2b3d4a16dd0b4e98788b178
Partha sarathi <parthasahoo2677@gmail.com>
2023-01-07 12:40:40 +0530

    first

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
